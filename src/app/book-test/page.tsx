62918594018cbd69eee3bf383cd9d520f35af376
Harsh <harshmkvviv@gmail.com>
2026-03-31 21:59:38 -0400

    Fix build: remove unused book-test route

--- src/app/book-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import InteractiveBook from '@/components/InteractiveBook';
-
-export default function BookTestPage() {
-  return (
-    <main style={{ width: '100vw', height: '100vh', backgroundColor: '#f0f0f0' }}>
-      <InteractiveBook />
-    </main>
-  );
-}
